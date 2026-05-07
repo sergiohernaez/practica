e8a9aa2a6a56c8c79e3e95bcfab65d0bb526e206
sergiohernaez <sergiohernaezc@gmail.com>
2026-05-07 09:55:23 +0200

    restored integration tests in jenkinsfile

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -4,18 +4,18 @@ from urllib.request import urlopen
 
 import pytest
 
-BASE_URL = "http://localhost:5000"
+#BASE_URL = "http://localhost:5000"
 BASE_URL_MOCK = "http://localhost:9090"
 DEFAULT_TIMEOUT = 2  # in secs
 
 @pytest.mark.api
 class TestApi(unittest.TestCase):
     def setUp(self):
-        self.assertIsNotNone(BASE_URL, "URL no configurada")
-        self.assertTrue(len(BASE_URL) > 8, "URL no configurada")
+        self.assertIsNotNone(BASE_URL_MOCK, "URL no configurada")
+        self.assertTrue(len(BASE_URL_MOCK) > 8, "URL no configurada")
 
     def test_api_add(self):
-        url = f"{BASE_URL}/calc/add/1/2"
+        url = f"{BASE_URL_MOCK}/calc/add/1/2"
         response = urlopen(url, timeout=DEFAULT_TIMEOUT)
         self.assertEqual(
             response.status, http.client.OK, f"Error en la petición API a {url}"
